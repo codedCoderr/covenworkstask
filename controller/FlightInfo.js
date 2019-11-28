62bbf1f3e6224c3851d9f021262e732a1d355e5e
codedcoderr <codedcoderrr@gmail.com>
2019-11-28 07:17:23 +0100

    add arrival and departure routes

--- controller/FlightInfo.js
+++ controller/FlightInfo.js
@@ -1,15 +1,40 @@
-const axios=require('axios')
+const axios = require('axios');
 const baseURL = 'https://codedcoder:busola@opensky-network.org/api';
 
-const flightInfo = async (req, res) => {
-  try {
-    const response = await axios.get(
-      `${baseURL}/`
-    );
-    console.log(response.data);
-    res.status(200).json(response.data);
-  } catch (error) {
-    console.log(error) ;
+const flightInfo = {
+  async arrival(req, res) {
+    try {
+      const { airport, begin, end } = req.body;
+      const response = await axios.get(
+        `${baseURL}/flights/arrival?airport=${airport}&begin=${begin}&end=${end}`
+      );
+      return res.json({
+        success: true,
+        data: response.data
+      });
+    } catch (error) {
+      return res.json({
+        success: false,
+        message: 'No values found for the specified time frame'
+      });
+    }
+  },
+  async departure(req, res) {
+    try {
+      const { airport, begin, end } = req.body;
+      const response = await axios.get(
+        `${baseURL}/flights/departure?airport=${airport}&begin=${begin}&end=${end}`
+      );
+      return res.json({
+        success: true,
+        data: response.data
+      });
+    } catch (error) {
+      return res.json({
+        success: false,
+        message: 'No values found for the specified time frame'
+      });
+    }
   }
 };
 
--- routes/FlightInfo.js
+++ routes/FlightInfo.js
@@ -3,6 +3,7 @@ const flight = require('../controller/FlightInfo');
 
 const router = express.Router();
 
-router.get('/flightInfo', flight);
+router.get('/arrival', flight.arrival);
+router.get('/departure', flight.departure);
 
 module.exports = router;
