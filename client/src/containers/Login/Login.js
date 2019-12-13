baa0549b8f3b68798872ae5f0a952fa8af922aaf
codedcoderr <codedcoderrr@gmail.com>
2019-12-13 16:51:36 +0100

    modify login

--- client/src/containers/Login/Login.js
+++ client/src/containers/Login/Login.js
@@ -7,13 +7,15 @@ import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
-// import Grid from '@material-ui/core/Grid';
+import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+// import Container from '@material-ui/core/Container';
 import { login } from '../../actions/auth';
+import Paper from '@material-ui/core/Paper';
+
 function Copyright() {
   return (
     <Typography variant='body2' color='textSecondary' align='center'>
@@ -34,13 +36,26 @@ const useStyles = makeStyles(theme => ({
     flexDirection: 'column',
     alignItems: 'center'
   },
-  body: {
-    backgroundImage:
-      'url(https://aviation.report/images/Resources/5269c0fb-7d95-4794-8622-9f78fc486332_Which-New.jpg)',
-    backgroundPosition: 'center',
-    backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat'
+  root: {
+    height: '100vh'
   },
+  image: {
+    backgroundImage:
+      'url(https://images.unsplash.com/photo-1520437358207-323b43b50729?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80)',
+    backgroundRepeat: 'no-repeat',
+    backgroundColor:
+      theme.palette.type === 'dark'
+        ? theme.palette.grey[900]
+        : theme.palette.grey[50],
+    backgroundSize: 'cover',
+    backgroundPosition: 'center'
+  },
+  // paper: {
+  //   margin: theme.spacing(8, 4),
+  //   display: 'flex',
+  //   flexDirection: 'column',
+  //   alignItems: 'center'
+  // },
   avatar: {
     margin: theme.spacing(1),
     backgroundColor: theme.palette.secondary.main
@@ -52,6 +67,7 @@ const useStyles = makeStyles(theme => ({
   submit: {
     margin: theme.spacing(3, 0, 2)
   }
+
 }));
 
 const Login = ({ login, history, loginAuth }) => {
@@ -74,9 +90,10 @@ const Login = ({ login, history, loginAuth }) => {
   const classes = useStyles();
 
   return (
-    <div className={classes.body}>
-      <Container className={classes.paper} component='main' maxWidth='xs'>
-        <CssBaseline />
+    <Grid container component='main' className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
         <div className={classes.paper}>
           <Avatar className={classes.avatar}>
             <LockOutlinedIcon />
@@ -122,7 +139,6 @@ const Login = ({ login, history, loginAuth }) => {
               control={<Checkbox value='remember' color='primary' />}
               label='Remember me'
             />
-
             {loading ? (
               <Button
                 disabled
@@ -140,13 +156,14 @@ const Login = ({ login, history, loginAuth }) => {
                 Sign In
               </Button>
             )}
+
+            <Box mt={8}>
+              <Copyright />
+            </Box>
           </form>
         </div>
-        <Box mt={8}>
-          <Copyright />
-        </Box>
-      </Container>
-    </div>
+      </Grid>
+    </Grid>
   );
 };
 const mapStateToProps = state => ({
